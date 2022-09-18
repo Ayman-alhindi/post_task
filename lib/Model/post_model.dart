96f97797962adb534385e355505e60cdc6b76d72
Laitooo <killuasan25@gmail.com>
2022-09-18 21:02:14 +0200

    instead of having to manually write the model, use build_runner with freezed

--- lib/Model/post_model.dart
+++ lib/Model/post_model.dart
@@ -1,73 +1,32 @@
-class PostDataModel {
-  PostDataModel({
-    required this.text,
-    required this.time,
-    required this.image,
-    required this.ownerName,
-    required this.likes,
-    required this.shares,
-    required this.comments,
-  });
+import 'package:freezed_annotation/freezed_annotation.dart';
 
-  late final String text;
-  late final String time;
-  late final String image;
-  late final String ownerName;
-  late final List<String> likes;
-  late int shares;
-  late final List<CommentDataModel> comments;
+part 'post_model.g.dart';
+part 'post_model.freezed.dart';
 
-  PostDataModel.fromJson(Map<String, dynamic> json) {
-    text = json['text'] ?? '';
-    time = json['time'] ?? '';
-    image = json['image'] ?? '';
-    ownerName = json['ownerName'] ?? '';
-    if (json['likes'] != null) {
-      likes = List.from(json['likes']).map((e) => e.toString()).toList();
-    } else {
-      likes = [];
-    }
-    shares = json['shares'] ?? 0;
-    comments = List.from(json['comments'])
-        .map((e) => CommentDataModel.fromJson(e))
-        .toList();
-  }
+@freezed
+class PostDataModel with _$PostDataModel {
+  factory PostDataModel({
+    required String text,
+    required String time,
+    required String image,
+    required String ownerName,
+    required List<String> likes,
+    required int shares,
+    required List<CommentDataModel> comments,
+  }) = _PostDataModel;
 
-  Map<String, dynamic> toJson() {
-    return {
-      'text': text,
-      'time': time,
-      'image': image,
-      'ownerName': ownerName,
-      'likes': likes.map((element) => element).toList(),
-      'shares': shares,
-      'comments': comments.map((element) => element.toJson()).toList(),
-    };
-  }
+  factory PostDataModel.fromJson(Map<String, dynamic> json) =>
+      _$PostDataModelFromJson(json);
 }
 
-class CommentDataModel {
-  CommentDataModel({
-    required this.text,
-    required this.time,
-    required this.ownerName,
-  });
+@freezed
+class CommentDataModel with _$CommentDataModel {
+  factory CommentDataModel({
+    required String text,
+    required String time,
+    required String ownerName,
+  }) = _CommentDataModel;
 
-  late final String text;
-  late final String time;
-  late final String ownerName;
-
-  CommentDataModel.fromJson(Map<String, dynamic> json) {
-    text = json['text'] ?? '';
-    time = json['time'] ?? '';
-    ownerName = json['ownerName'] ?? '';
-  }
-
-  Map<String, dynamic> toJson() {
-    return {
-      'text': text,
-      'time': time,
-      'ownerName': ownerName,
-    };
-  }
+  factory CommentDataModel.fromJson(Map<String, dynamic> json) =>
+      _$CommentDataModelFromJson(json);
 }
